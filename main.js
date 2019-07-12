cb7eec4cd289870b4a356a291c9c387b047a943f
Daianes26 <daianeasilva26@gmail.com>
2019-07-12 14:26:41 -0400

    Player working

--- main.js
+++ main.js
@@ -11,31 +11,46 @@ function musicNode(music){
     musicDiv.classList.add('music')
     musicDiv.innerHTML = `
         <h3>${music.trackName}</h3>
-        <p>${music.collectionName}</p>`
+        <p>Album: ${music.collectionName}</p>
+        <img src="${music.artworkUrl100}">
+        <p><input class="playback br2" id="playback" type="button" src="${music.previewUrl}" value="Play"></p>`
+        // console.log(music.previewUrl)
     
     return musicDiv
 
 }
 
-function displayMusicData (musicUrl){
-    fetch(musicUrl)
-    .then(res => res.json())
-    .then(function (data){
-        const dataDisplay = q('#music-data')
-        dataDisplay.innerHTML = `
-            <h3> More info about ${data.artistName}</h3>`
+const playSong = document.querySelector("#audio")
+const songSource = document.querySelector('#music-source')
 
-    })
-}
+
+// function playMusic(){
+//     songSource.src = playback.src
+//     playSong.load()
+//     songSource.play()
+
+// }
+
+// function displayMusicData (musicUrl){
+//     fetch(musicUrl)
+//     .then(res => res.json())
+//     .then(function (data){
+//         const dataDisplay = q('#music-data')
+//         dataDisplay.innerHTML = `
+//             <h3> More info about ${data.artistName}</h3>`
+
+//     })
+// }
 
 document.addEventListener('DOMContentLoaded', function(){
-    // q('#music-results').addEventListener('click', function(event){
-    //     console.log(event.target)
-        // if (event.target && event.target.matches()){
-
-        // }
-        // // Add music event
-    // })
+    q('#music-results').addEventListener('click', function(event){
+        if (event.target && event.target.matches('#playback')){
+            songSource.src = event.target.src
+            playSong.load()
+            playSong.play()
+        
+        }
+    })
 
     q('form').addEventListener('submit', function (event){
         event.preventDefault()
